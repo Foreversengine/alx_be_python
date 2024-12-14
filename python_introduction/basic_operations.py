8dbc7ef400ff1be80d8f3dfb4e1451845b7095dc
Foreversengine <opeloyeruwasiu123@gmail.com>
2024-12-14 00:52:22 +0000

    Calculation of area of a rectangle

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+#Define the length and breadth of the rectangle
+Length = 10
+Breadth = 5
+
+#Calculate the area of the rectangle
+Area = Length * Breadth
+
+#Print the calculated area
+print(f"The Area of the rectangle is {50}")
