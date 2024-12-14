d76534817407c27c2da7bcb98ab6b2a7eeb97f73
Foreversengine <opeloyeruwasiu123@gmail.com>
2024-12-14 01:05:03 +0000

    Area, Addition and subtraction of area of a rectangle

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,9 +1,13 @@
 #Define the length and breadth of the rectangle
-Length = 10
-Breadth = 5
+number1 = 10
+number2 = 5
 
 #Calculate the area of the rectangle
-Area = Length * Breadth
+Area = number1 * number2
 
 #Print the calculated area
 print(f"The Area of the rectangle is {50}")
+
+#Addition and subtraction of these numbers
+addition = length + width
+subtraction = length - width
