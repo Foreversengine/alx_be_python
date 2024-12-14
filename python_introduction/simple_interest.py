131a6dff518b56f861a76b6644c445c0cc98de3e
Foreversengine <opeloyeruwasiu123@gmail.com>
2024-12-14 01:41:59 +0000

    Interest value

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#Calculate the simple interest on a given principal amount, rate of interest, and time.
+principal = 1000
+rate = 0.05
+time = 3
+
+#Calculatiion for the simple interest
+interest = principal * rate * time 
+
+#Print interest value
+print("The simple interest is: {interest}")
