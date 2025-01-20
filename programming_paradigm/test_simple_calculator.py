f30470f02979779741da0196d9c7d0e541229368
Foreversengine <opeloyeruwasiu123@gmail.com>
2025-01-20 12:13:16 +0000

    Writing Unit tests for a Simple Calculator class

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,25 +1,32 @@
-# simple_calculator.py
+import unittest
+from simple_calculator import SimpleCalculator
 
-class SimpleCalculator:
-    """A simple calculator class that supports basic arithmetic operations."""
-
-    def add(self, a, b):
-        """Return the addition of a and b."""
-        return a + b
-
-    def subtract(self, a, b):
-        """Return the subtraction of b from a."""
-        return a - b
-
-    def multiply(self, a, b):
-        """Return the multiplication of a and b."""
-        return a * b
-
-    def divide(self, a, b):
-        """Return the division of a by b. Returns None if b is zero."""
-        if b == 0:
-            return None
-        return a / b
+class TestSimpleCalculator(unittest.TestCase):
     
+    
+    def setUp(self):
+        self.calc = SimpleCalculator()
+    
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
+        
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(2, 3), -1)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+    
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(2,3), 6)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
+    
+    def test_division(self):
+        self.assertEqual(self.calc.divide(6, 2), 3)
+        self.assertEqual(self.calc.divide(-6, 2), -3)
+        self.assertEqual(self.calc.divide(-6, -2), 3)
+        
 
-simple_calculator = SimpleCalculator()
+         # Test dividing by zero
+        self.assertIsNone(self.calc.divide(10, 0))
